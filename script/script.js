0d8d51e84cee4b882840dc3d8fa03839d6c179d1
Fer Illanes <illanes.f.m@gmail.com>
2023-06-03 18:21:54 -0300

    subiendo la preentrega

---
+++ script/script.js
@@ -0,0 +1,47 @@
+
+function juego() {
+    let numeroAdivinar
+    do {
+        numeroAdivinar = parseInt(prompt("Jugador uno ingresa un numero sin que el Jugador dos lo vea" + "\n" + "el numero debe ser entero del 1 al 10"))
+    } while (isNaN(numeroAdivinar) || numeroAdivinar < 1 || numeroAdivinar > 10);
+
+    alert("¡Jugador dos, comienza a adivinar!")
+
+    let intentos = 3
+
+    for (let i = 3; i > 0; i--) {
+        let numeroAdivinado = parseInt(prompt("Adivina el numero!"))
+        if (numeroAdivinado === numeroAdivinar) {
+            alert("Felicidades Jugador dos, has ganado el juego")
+            return
+        } else {
+            intentos--
+
+            if (intentos === 0) {
+                alert("Lo siento Jugador dos, has perdido el juego." + "\n" + " El numero correcto era " + numeroAdivinar)
+            } else {
+                alert("Lo lamento, el numero no es correcto, vuelve a intentar" + "\n" + intentos + " intentos restantes")
+            }
+        }
+    }
+
+    juegoBienvenida()
+}
+
+juegoBienvenida()
+
+function juegoBienvenida() {
+    let bienvenida
+    do {
+        let mensaje = "bienvenidos al juego de ADIVINA EL NUMERO" + "\n" + "Presiona: 1 para jugar, 0 para salir"
+        bienvenida = prompt(mensaje)
+
+    } while (bienvenida != 1 && bienvenida != 0)
+
+    if (bienvenida === "1") {
+        juego()
+    } else {
+        alert("Adios!, vuelve a jugar cuando quieras!")
+    }
+}
+
